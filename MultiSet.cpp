282ee4615ac1061ab36ddfcab6255332fa5ed039
vladimirggg <vladimir.grig02@gmail.com>
2024-04-16 12:11:10 +0300

    Bug fixes in add and occurrenceCount methods.

---
+++ MultiSet.cpp
@@ -0,0 +1,165 @@
+#include "MultiSet.h"
+#include <fstream>
+
+uint8_t MultiSet::MAX_VALUES[8] = {
+    0b11111111,
+    0b01111111,
+    0b00111111,
+    0b00011111,
+    0b00001111,
+    0b00000111,
+    0b00000011,
+    0b00000001
+};
+size_t MultiSet::NUMBER_OF_BITS = 8;
+
+MultiSet::MultiSet(): numberSet(nullptr), bucketSize(0), maxNumber(0){}
+
+MultiSet::MultiSet(unsigned int n, unsigned int k) : maxNumber(n) {
+    if(k < 1 || k > 8) throw "K out of bounds!"; //TODO find a way to skip the exception
+    else bucketSize = k;
+    const size_t arraySize = getArraySize(); // 8 bit rounded up
+    numberSet = new uint8_t[arraySize]; // round up
+}
+
+MultiSet::MultiSet(const MultiSet& other) {
+    copyFrom(other);
+}
+
+MultiSet& MultiSet::operator=(const MultiSet& other) {
+    if(this != &other){
+        free();
+        copyFrom(other);
+    }
+    return *this;
+}
+
+void MultiSet::copyFrom(const MultiSet &other) {
+    bucketSize = other.bucketSize;
+    maxNumber = other.maxNumber;
+    const size_t arraySize = getArraySize();
+    numberSet = new uint8_t[arraySize];
+    for (int i = 0; i <= arraySize; ++i)
+        numberSet[i] = other.numberSet[i];
+}
+
+void MultiSet::free() {
+    maxNumber = 0;
+    bucketSize = 0;
+    delete[] numberSet;
+}
+
+size_t MultiSet::getArraySize() const {
+    return ((maxNumber * bucketSize) / 8) + 1;
+}
+
+void MultiSet::add(unsigned int number) {
+    if(number > maxNumber) throw "Number out of bounds!";
+    if(occurrenceCount(number) == MultiSet::MAX_VALUES[MultiSet::NUMBER_OF_BITS - bucketSize]) return;
+
+    size_t clusterIndex = ((number - 1) * bucketSize) / MultiSet::NUMBER_OF_BITS;
+    size_t bitIndex = ((number - 1) * bucketSize) % MultiSet::NUMBER_OF_BITS;
+
+    if((numberSet[clusterIndex] >> bitIndex) == MultiSet::MAX_VALUES[MultiSet::NUMBER_OF_BITS - bitIndex]){
+        numberSet[clusterIndex] &= ~(MultiSet::MAX_VALUES[MultiSet::NUMBER_OF_BITS - bitIndex] << bitIndex);
+        //clear the last bits
+        numberSet[clusterIndex + 1] += 1;
+
+    }
+    else{
+        uint8_t mask = 1 << bitIndex;
+        numberSet[clusterIndex] += mask;
+    }
+}
+
+uint8_t MultiSet::occurrenceCount(unsigned int number) const {
+    size_t clusterIndex = ((number - 1) * bucketSize) / MultiSet::NUMBER_OF_BITS;
+    size_t bitIndex = ((number - 1) * bucketSize) % MultiSet::NUMBER_OF_BITS;
+    uint8_t occurrenceMask = numberSet[clusterIndex] >> bitIndex;
+
+    if (bitIndex + bucketSize > MultiSet::NUMBER_OF_BITS) {
+        uint8_t remainingBits = bucketSize - (MultiSet::NUMBER_OF_BITS - bitIndex);
+
+        occurrenceMask |= (numberSet[clusterIndex + 1] & ((1 << remainingBits) - 1)) << (MultiSet::NUMBER_OF_BITS - bitIndex);
+    }
+
+    occurrenceMask &= MAX_VALUES[MultiSet::NUMBER_OF_BITS - bucketSize];
+
+    return occurrenceMask;
+}
+
+void MultiSet::printNumbers() const {
+    for (size_t i = 1; i <= maxNumber; i++)
+        if(occurrenceCount(i)) std::cout<<i<<std::endl;
+}
+
+void MultiSet::printMultiSetInMemory() const {
+    const size_t arraySize = getArraySize();
+    for (size_t i = 0; i < arraySize; ++i) {
+        for (int j = 7; j >= 0; --j) {
+            std::cout << ((numberSet[i] >> j) & 1);
+        }
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+}
+
+void MultiSet::serialize(const char* fileName) const{
+    std::ofstream file(fileName, std::ios::binary);
+    if(!file){
+        std::cout<<"Error opening output file!\n";
+        return;
+    }
+    const size_t arraySize = getArraySize();
+    file.write((const char*) &maxNumber, sizeof(unsigned int));
+    file.write((const char*) &bucketSize, sizeof(size_t));
+    file.write((const char*) numberSet, arraySize * sizeof(uint8_t));
+    file.close();
+}
+
+void MultiSet::deserialize(const char* fileName){
+    std::ifstream file(fileName);
+    if(!file){
+        std::cout<<"Error opening input file!\n";
+        return;
+    }
+    free();
+    file.read((char*) &maxNumber, sizeof(unsigned int));
+    file.read((char*) &bucketSize, sizeof(size_t));
+    const size_t arraySize = getArraySize();
+    numberSet = new uint8_t[arraySize];
+    file.read((char*) numberSet, arraySize * sizeof(uint8_t));
+}
+
+MultiSet MultiSet::intersection(const MultiSet& other) const {
+    MultiSet intersection(std::min(maxNumber,other.maxNumber), 
+                        std::min(bucketSize, other.bucketSize));
+    const size_t arraySize = intersection.getArraySize();
+    for (size_t i = 0; i < arraySize; i++) {
+        intersection.numberSet[i] = (numberSet[i] & other.numberSet[i]);
+    }
+    return intersection;
+}
+
+MultiSet MultiSet::difference(const MultiSet& other) const {
+    MultiSet difference(std::min(maxNumber,other.maxNumber), 
+                        std::min(bucketSize, other.bucketSize));
+    const size_t arraySize = difference.getArraySize();
+    for (size_t i = 0; i < arraySize; i++) {
+        difference.numberSet[i] = (numberSet[i] ^ other.numberSet[i]);
+    }
+    return difference;
+}
+
+MultiSet MultiSet::fillInMultiSet() const {
+    MultiSet filledIn(*this);
+    const size_t arraySize = getArraySize();
+    for (size_t i = 0; i < arraySize; i++){
+        filledIn.numberSet[i] = ~filledIn.numberSet[i];
+    }
+    return filledIn;
+}
+
+MultiSet::~MultiSet() {
+    free();
+}
---
+++ MultiSet.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+class MultiSet {
+    static uint8_t MAX_VALUES[8];
+    static size_t NUMBER_OF_BITS;
+    uint8_t* numberSet;
+    size_t bucketSize;
+    unsigned int maxNumber;
+
+    void copyFrom(const MultiSet& other);
+    void free();
+    size_t getArraySize() const;
+public:
+    MultiSet();
+    MultiSet(unsigned int n, unsigned int k);
+    MultiSet(const MultiSet& other);
+    MultiSet& operator= (const MultiSet& other);
+    void add(unsigned int number);
+    uint8_t occurrenceCount(unsigned int number) const;
+    void printNumbers() const;
+    void printMultiSetInMemory() const;
+    void serialize(const char* fileName) const; // out
+    void deserialize(const char* fileName); // in
+    MultiSet intersection(const MultiSet& other) const;
+    MultiSet difference(const MultiSet& other) const;
+    MultiSet fillInMultiSet() const;
+    ~MultiSet();
+};
